dbdf305d7110424b9afd81265121c69d6d8896aa
alsami <al.khatib.sami1990@gmail.com>
2020-10-03 10:35:00 +0200

    chore: upgrade package for compatibility with Autofac 6

--- src/Autofac.Extras.AggregateService/Properties/AggregateServicesResources.cs
+++ src/Autofac.Extras.AggregateService/Properties/AggregateServicesResources.cs
@@ -6,12 +6,11 @@ namespace Autofac.Extras.AggregateService.Properties
 {
     internal static class AggregateServicesResources
     {
-        private static readonly ResourceManager _resourceManager
-            = new ResourceManager("Autofac.Extras.AggregateService.Properties.AggregateServicesResources", typeof(AggregateServicesResources).GetTypeInfo().Assembly);
+        private static readonly ResourceManager ResourceManager
+            = new ResourceManager(
+                "Autofac.Extras.AggregateService.Properties.AggregateServicesResources",
+                typeof(AggregateServicesResources).GetTypeInfo().Assembly);
 
-        public static string GetString(string name)
-        {
-            return _resourceManager.GetString(name, CultureInfo.CurrentCulture);
-        }
+        public static string GetString(string name) => ResourceManager.GetString(name, CultureInfo.CurrentCulture);
     }
 }
