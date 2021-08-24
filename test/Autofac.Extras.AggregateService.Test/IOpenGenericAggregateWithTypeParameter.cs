adec579e7ba773d39d0c6548bdf42bfce8b7d3bf
Stijn Van Antwerpen <stijn.vanantwerpen@gmail.com>
2021-08-24 22:00:30 +0200

    https://github.com/autofac/Autofac.Extras.AggregateService/issues/5

---
+++ test/Autofac.Extras.AggregateService.Test/IOpenGenericAggregateWithTypeParameter.cs
@@ -0,0 +1,8 @@
+﻿namespace Autofac.Extras.AggregateService.Test
+{
+    public interface IOpenGenericAggregateWithTypeParameter<T>
+    {
+        T SomeProperty { get; }
+        IOpenGeneric<T> OpenGeneric { get; }
+    }
+}
